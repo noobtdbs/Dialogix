f1cef009b9c7081c5df569bc989c50a6a8620c14
noobtdbs <LCB2021002@IIITL.AC.IN>
2023-08-30 15:51:42 +0530

    initial commit

---
+++ chatbot/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin 
+from .models import Chat
+
+admin.site.register(Chat)
+
+'''
+database:
+username = admin
+password = admin
+'''
+
+
+
+# Register your models here.
